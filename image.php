b89730e7e08bfb457c0846ced1b0e72ec7ed0751
Gordon Brander <gordon@crowdfavorite.com>
2008-12-09 22:50:12 +0000

    PNG images with transparent backgrounds. Adding IE6 stylesheets for image gallery. Creating new IE images directory. Cleaning up repository.

--- image.php
+++ image.php
@@ -19,6 +19,7 @@ if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
 if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes() ?>>
 <head profile="http://gmpg.org/xfn/11">
@@ -33,7 +34,9 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 	
 	<link rel="stylesheet" type="text/css" media="screen, print, handheld" href="<?php bloginfo('template_url') ?>/css/typography.css" />
 	<link rel="stylesheet" type="text/css" media="screen" href="<?php bloginfo('template_directory') ?>/css/image.css" />
-
+	<!--[if lte IE 6]>
+		<link rel="stylesheet" type="text/css" media="screen" href="<?php bloginfo('template_directory') ?>/css/image-ie6.css" />
+	<![endif]-->
 	<?php wp_head(); ?>
 </head>
 
