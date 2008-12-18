4e4faee476863c0b0b299b1796f012410d96e774
Gordon Brander <gordon@crowdfavorite.com>
2008-12-18 16:07:36 +0000

    Tweaking markup and CSS to share the standard classnames from the new 2.7 comments function. New single-column comment style that better meshes with threaded comments.

--- comment/comment-default.php
+++ comment/comment-default.php
@@ -21,7 +21,7 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 global $comment;
 
 ?>
-<div id="comment-<?php comment_ID(); ?>" class="<?php cfct_comment_class(); ?>">
+<div id="div-comment-<?php comment_ID(); ?>">
 <?php
 if ($comment->comment_approved == '0') {
 ?>
@@ -30,22 +30,25 @@ if ($comment->comment_approved == '0') {
 }
 ?>
 
-	<div class="info">
-		<address class="vcard author entry-title comment-author">
+	<address class="vcard author entry-title comment-author">
 <?php 
 if (function_exists('get_avatar')) { 
 ?>
-			<span class="photo"><?php echo get_avatar($comment, 36) ?></span>
+		<span class="photo avatar"><?php echo get_avatar($comment, 54) ?></span>
 <?php
 }
 add_filter('get_comment_author_link', 'cfct_hcard_comment_author_link');
-
-printf(__('%s <span class="said">said</span>', 'carrington'), get_comment_author_link());
-
+printf(__('%s <span class="said">said</span>', 'carrington'), '<cite class="fn">'.get_comment_author_link().'</cite>');
 remove_filter('get_comment_author_link', 'cfct_hcard_comment_author_link');
 ?>
-		</address><!--.vcard-->
-		<div class="date comment-date">
+	</address><!--.vcard-->
+
+	<div class="entry-content comment-content">
+		<?php comment_text() ?>
+	</div><!--.entry-content-->
+	
+	<div class="comment-meta commentmetadata">
+		<span class="date comment-date">
 <?php
 printf(
 	__('<span class="on">on</span> <abbr class="published" title="%s"><a title="Permanent link to this comment" rel="bookmark" href="%s#comment-%s">%s</a></abbr>'
@@ -57,11 +60,7 @@ printf(
 	, get_comment_date()
 );
 ?>
-		</div><!--.date-->
-	</div><!--.info-->
-
-	<div class="entry-content comment-content">
-		<?php comment_text() ?>
-	</div><!--.entry-content-->
-	<?php edit_comment_link(__('Edit This', 'carrington'), '<div class="comment-editlink">', '</div>'); ?>
+		</span><!--.date-->
+		<?php edit_comment_link(__('Edit This', 'carrington'), '<div class="comment-editlink">', '</div>'); ?>
+	</div>
 </div><!--.comment-->
--- comments/comments-loop.php
+++ comments/comments-loop.php
@@ -21,12 +21,12 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 global $comments, $comment;
 
 ?>
-	<ol class="comments-list hfeed">
+	<ol class="commentlist hfeed">
 <?php
 foreach ($comments as $comment) {
 	if (get_comment_type() == 'comment') {
 ?>
-		<li class="hentry <?php cfct_comment_list_class(); ?>">
+		<li class="hentry comment <?php cfct_comment_list_class(); ?>">
 <?php
 		cfct_comment();
 ?>
