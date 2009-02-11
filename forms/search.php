adaa3ccbdcf910ed2c06932330c81a19d194e23f
Gordon Brander <gordon@crowdfavorite.com>
2009-02-11 22:34:03 +0000

    Updated bloginfo function in search. Added clearing zoom to entry content in ie sheet. Update to border-solid style for IE on comments.

--- forms/search.php
+++ forms/search.php
@@ -27,7 +27,7 @@ else {
 
 ?>
 
-<form method="get" id="cfct-search" action="<?php echo trailingslashit(get_bloginfo('home')); ?>" onsubmit="<?php echo $onsubmit; ?>">
+<form method="get" id="cfct-search" action="<?php echo trailingslashit(get_bloginfo('url')); ?>" onsubmit="<?php echo $onsubmit; ?>">
 	<div>
 		<input type="text" id="cfct-search-input" name="s" value="<?php echo wp_specialchars($s, 1); ?>" size="15" />
 		<input type="submit" name="submit_button" value="<?php _e('Search', 'carrington-blog'); ?>" />
