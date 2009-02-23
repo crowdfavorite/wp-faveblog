8869f7b0f91a0aea1b9d53fd0d9b5921ed20385c
Alex King <alex@crowdfavorite.com>
2009-02-23 07:36:20 +0000

    helpful hints

--- image.php
+++ image.php
@@ -18,6 +18,8 @@
 if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
 if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 
+// NOTE: this file is here for compatibility reasons - active templates are in the image/ dir 
+
 cfct_image();
 
 ?>
