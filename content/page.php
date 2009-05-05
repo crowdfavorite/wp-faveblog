c3c832de1fa91561cc86df1341da33247475e395
Gordon Brander <gordon@crowdfavorite.com>
2009-05-05 16:04:28 +0000

    Converted "list_pages" to "wp_list_pages" and added clearing div (content already cleared, but prevents rare guillotine bug)

--- content/page.php
+++ content/page.php
@@ -26,7 +26,16 @@ $previousday = -1;
 	<h1 class="entry-title full-title"><?php the_title() ?></h1>
 	
 	<div class="entry-content full-content">
-		<?php the_content('<span class="more-link">'.__('Continued...', 'carrington-blog').'</span>'); link_pages('<p class="pages-link">'.__('Pages: ', 'carrington-blog'), "</p>\n", 'number'); ?>
+<?php 
+		the_content('<span class="more-link">'.__('Continued...', 'carrington-blog').'</span>'); 
+		$args = array(
+			'before' => '<p class="pages-link">'. __('Pages: ', 'carrington-blog'),
+			'after' => "</p>\n",
+			'next_or_number' => 'number'
+		);
+		wp_link_pages($args);
+?>
+		<div class="clear"></div>
 	</div><!--/entry-content-->
 	
 	<p class="by-line">
