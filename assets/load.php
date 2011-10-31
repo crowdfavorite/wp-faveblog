13fef82ed918ead074ae52431ab186ee6acfaa11
Gordon Brander <gordon@crowdfavorite.com>
2011-10-31 22:56:19 +0000

    Moved assets to assets directory.

--- assets/load.php
+++ assets/load.php
@@ -1,9 +1,10 @@
 <?php
-$url = get_template_directory_uri();
+$url = trailingslashit(get_template_directory_uri());
+define('CFCT_ASSETS_URL', $url.'assets/');
 
 wp_enqueue_style(
 	'carrington',
-	$url.'/css/css.php',
+	CFCT_ASSETS_URL.'css/css.php',
 	array(),
 	CFCT_VER
 );
@@ -12,7 +13,7 @@ wp_enqueue_script('jquery');
 
 wp_enqueue_script(
 	'carrington',
-	$url.'/js/carrington.js',
+	CFCT_ASSETS_URL.'js/carrington.js',
 	array('jquery'),
 	CFCT_VER
 );
@@ -23,14 +24,14 @@ if (cfct_get_option('cfct_ajax_load') == 'yes') {
 if (cfct_get_option('cfct_lightbox') != 'no') {
 	wp_enqueue_script(
 		'cfct_thickbox',
-		$url.'/carrington-core/lightbox/thickbox.js',
+		$url.'carrington-core/lightbox/thickbox.js',
 		array('jquery'),
 		CFCT_VER
 	);
 	
 	wp_enqueue_style(
 		'cfct_thickbox',
-		$url.'/carrington-core/lightbox/css/thickbox.css',
+		$url.'carrington-core/lightbox/css/thickbox.css',
 		array(),
 		CFCT_VER
 	);
@@ -43,7 +44,7 @@ function cfct_blog_head() {
 	
 	echo '
 <script type="text/javascript">
-var CFCT_URL = "'.get_bloginfo('url').'";
+var CFCT_URL = "'.home_url().'";
 var CFCT_AJAX_LOAD = '.$ajax_load.';
 </script>
 	';
@@ -51,7 +52,7 @@ var CFCT_AJAX_LOAD = '.$ajax_load.';
 	if (cfct_get_option('cfct_lightbox') != 'no') {
 		echo '
 <script type="text/javascript">
-tb_pathToImage = "' . get_bloginfo('template_directory') . '/carrington-core/lightbox/img/loadingAnimation.gif";
+tb_pathToImage = "' . get_template_directory_uri() . '/carrington-core/lightbox/img/loadingAnimation.gif";
 jQuery(function($) {
 	$("a.thickbox").each(function() {
 		var url = $(this).attr("rel");
@@ -162,19 +163,19 @@ a:visited {
 }
 #footer p#developer-link a,
 #footer p#developer-link a:visited {
-	background-image: url(<?php bloginfo('template_directory'); ?>/img/footer/by-crowd-favorite-<?php echo $footer_img_type; ?>.png);
+	background-image: url(<?php echo CFCT_ASSETS_URL; ?>img/footer/by-crowd-favorite-<?php echo $footer_img_type; ?>.png);
 }
 <?php
 	if (cfct_get_option('cfct_css_background_images') != 'no') {
 ?>
 #header {
-	background-image: url(<?php bloginfo('template_directory'); ?>/img/header/gradient-<?php echo $header_grad_type; ?>.png);
+	background-image: url(<?php echo CFCT_ASSETS_URL; ?>img/header/gradient-<?php echo $header_grad_type; ?>.png);
 }
 #header .wrapper {
-	background-image: url(<?php bloginfo('template_directory'); ?>/img/header/texture-<?php echo $header_img_type; ?>.png);
+	background-image: url(<?php echo CFCT_ASSETS_URL; ?>img/header/texture-<?php echo $header_img_type; ?>.png);
 }
 #footer {
-	background-image: url(<?php bloginfo('template_directory'); ?>/img/footer/gradient-<?php echo $footer_img_type; ?>.png);
+	background-image: url(<?php echo CFCT_ASSETS_URL; ?>img/footer/gradient-<?php echo $footer_img_type; ?>.png);
 }
 <?php
 	}
